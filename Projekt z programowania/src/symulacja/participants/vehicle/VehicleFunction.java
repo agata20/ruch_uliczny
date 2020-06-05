8e01557940e5ff5a142c7528dba81daa7ad4c844
agata20 <65370332+agata20@users.noreply.github.com>
2020-06-05 13:11:52 +0200

    Update VehicleFunction.java

--- Projekt z programowania/src/symulacja/participants/vehicle/VehicleFunction.java
+++ Projekt z programowania/src/symulacja/participants/vehicle/VehicleFunction.java
@@ -9,7 +9,5 @@ public interface VehicleFunction extends RandomPositionOccupation {
     void accelerate(int speed, int maxSpeed);
     void slowDown(int speed, int safeSpeed);
     PositionOccupation seeObstacle(int coordinateX, int coordinateY);
-    public void move(int maxSpeed) throws InterruptedException;
-
-
+    public void move(int speed) throws InterruptedException;
 }
